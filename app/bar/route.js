45a195cf174acffc7c3c4a2080708ff846b92b20
Jimmy Lai <laijimmy0@gmail.com>
2023-06-13 10:33:25 +0200

    add hello world

---
+++ app/bar/route.js
@@ -0,0 +1,5 @@
+export function GET() {
+  return new Response("Hello Bar!");
+}
+
+export const dynamic = "force-dynamic";
---
+++ app/foo/route.js
@@ -0,0 +1,5 @@
+export function GET() {
+  return new Response("Hello Foo!");
+}
+
+export const dynamic = "force-dynamic";
